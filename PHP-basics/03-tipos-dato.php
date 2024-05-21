a00bdcf9a39047fa658835557759ef628c2f00e3
SaraRFraga <163723468+SaraRFraga@users.noreply.github.com>
2024-05-21 12:39:10 +0200

    Add files via upload

---
+++ PHP-basics/03-tipos-dato.php
@@ -0,0 +1,23 @@
+<?php include 'includes/header.php';
+
+// Boolean: datos que solo pueden ser true o false
+$logueado = true;
+var_dump($logueado);
+
+// Enteros
+$numero = 200;
+var_dump($numero);
+
+// Floats: números no enteros
+$float = 200.5;
+var_dump($float);
+
+// Strings: cadenas de texto
+$nombre = "Zeky";
+var_dump($nombre);
+
+$array = [];
+var_dump($array);
+
+
+include 'includes/footer.php';
